b4659df3fef267c665a5caf71dafd272ffb96fbb
ivainqueur <ishimvainqueur@gmail.com>
2023-11-09 20:46:38 +0200

    Add error handling and logging for requests

--- index.js
+++ index.js
@@ -13,34 +13,39 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.all("/*", (req, res) => {
     const url = req.originalUrl.slice(1).replace(/%20/g, "");
 
-    request(
-        {
-            url: url,
-            method: req.method,
-            json: req.body,
-            headers: {
-                Authorization: req.header("Authorization"),
-                "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
-            },
-            timeout: 10000,
-            withCredentials: true,
+    try {
+        request(
+            {
+                url: url,
+                method: req.method,
+                json: req.body,
+                headers: {
+                    Authorization: req.header("Authorization"),
+                    "User-Agent":
+                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+                },
+                timeout: 10000,
+                withCredentials: true,
 
-        },
-        function (error, response, body) {
-            if (error) {
-                console.error("📢 error: " + error);
+            },
+            function (error, response, body) {
+                if (error) {
+                    console.error("📢 error: " + error);
+                    return res.status(500).json({ code: "#Error", message: error.message })
+                }
             }
-        }
-    ).pipe(res);
+        ).pipe(res);
+    } catch (error) {
+        return res.status(500).json({ code: "#Error", message: error.message })
+    }
 });
 
-app.use('*', (req, res)=>{
+app.use('*', (req, res) => {
     console.log("[log] Undocumented route")
-    res.json({code: "#Undocumented", message: "Nothing on this route yet"})
+    res.json({ code: "#Undocumented", message: "Nothing on this route yet" })
 })
 
 
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT, () => {
     console.log("[log]: Server is up at PORT ", process.env.PORT)
 })
